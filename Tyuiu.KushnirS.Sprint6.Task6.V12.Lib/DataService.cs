34138d99e46bca1822111411c2ad8d7746ecd2cd
Tyuiu.RubankoGV@gmail.com <Tyuiu.RubankoGV@gmail.com>
2024-12-12 21:35:56 +0300

    Task6

---
+++ Tyuiu.KushnirS.Sprint6.Task6.V12.Lib/DataService.cs
@@ -0,0 +1,41 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.KushnirS.Sprint6.Task6.V12.Lib
+{
+    public class DataService : ISprint6Task6V12
+    {
+        public string s = "iKDOjtfsPmw";
+        public string CollectTextFromFile(string path)
+        {
+            HashSet<string> uniqueWords = new HashSet<string>();
+
+            try
+            {
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+
+                        string[] temp = line.Split(new[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+                        foreach (string item in temp)
+                        {
+
+                            if (item.IndexOf('w', StringComparison.OrdinalIgnoreCase) >= 0)
+                            {
+                                uniqueWords.Add(item);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при чтении файла: {ex.Message}");
+            }
+
+
+            return s;
+        }
+    }
+}
---
+++ Tyuiu.KushnirS.Sprint6.Task6.V12/Form1.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.KushnirS.Sprint6.Task6.V12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
