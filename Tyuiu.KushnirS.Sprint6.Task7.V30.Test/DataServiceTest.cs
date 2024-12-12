035f4ca9b251ae1cd013a01bc1e6b634bbb18d67
Tyuiu.RubankoGV@gmail.com <Tyuiu.RubankoGV@gmail.com>
2024-12-12 21:38:44 +0300

    Task7

---
+++ Tyuiu.KushnirS.Sprint6.Task7.V30.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.KushnirS.Sprint6.Task7.V30.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
